a307174738ec07b962629a8bd8f04e7395178aa7
Leto <leto.developer@gmail.com>
2020-02-09 16:51:02 +0100

    [feat][style] rework to match design of Huawei

--- app/src/main/java/herwig_l/calculator/MainActivity.kt
+++ app/src/main/java/herwig_l/calculator/MainActivity.kt
@@ -2,71 +2,163 @@ package herwig_l.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
 
-    private var numParentheses: Int = 0
+//    private var numParentheses: Int = 0
+
+    private var memory: Double = 0.0
+    private var hasMemory: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnEquals.setOnClickListener {
-            try {
-                val equation = ExpressionBuilder(tvEquation.text.toString()).build()
-                val result = equation.evaluate()
-                tvResult.text = result.toString()
-            } catch (e: Exception) {
-                tvResult.text = "ERROR"
+            val result = calculateResult()
+            if (result !== null) {
+                tvEquation.text = result.toString()
+                tvResult.text = ""
+            }
+        }
+        btnMemAdd.setOnClickListener {
+            val result = calculateResult()
+            if (result !== null) {
+                hasMemory = true
+                memory += result
+            }
+        }
+        btnMemSubract.setOnClickListener {
+            val result = calculateResult()
+            if (result !== null) {
+                hasMemory = true
+                memory -= result
+            }
+        }
+        btnMemClear.setOnClickListener {
+            hasMemory = false
+            memory = 0.0
+        }
+        btnMemLoad.setOnClickListener {
+            if (hasMemory) {
+                if (tvEquation.text.toString().isEmpty()) {
+                    tvEquation.text = tvEquation.text.toString() + memory.toString()
+                } else {
+
+                    val lastSymbol = tvEquation.text.toString().takeLast(1)
+                    Log.d("MemLoad", lastSymbol)
+
+                    if (lastSymbol !== "+" && lastSymbol !== "-" && lastSymbol !== "*" && lastSymbol !== "/") {
+                        if (memory < 0) {
+                            tvEquation.text = tvEquation.text.toString() + memory.toString()
+                        } else if (memory >= 0) {
+                            tvEquation.text = tvEquation.text.toString() + "+" + memory.toString()
+                        }
+                    }
+                    tvResult.text = calculateResult()?.toString() ?: ""
+                }
             }
         }
 
-        btnClear.setOnClickListener               { tvEquation.text = ""; tvResult.text = "" }
-        btnBack.setOnClickListener                { tvEquation.text = tvEquation.text.dropLast(1) }
-        btnAdd.setOnClickListener                 { addSymbolToEquation("+", true) }
-        btnSubtract.setOnClickListener            { addSymbolToEquation("-", true) }
-        btnMultiply.setOnClickListener            { addSymbolToEquation("*", true) }
-        btnDivid.setOnClickListener               { addSymbolToEquation("/", true) }
-        btnOpenParenthesis.setOnClickListener     { addSymbolToEquation("(") }
-        btnClosingParenthesis.setOnClickListener  { addSymbolToEquation(")") }
-        btnPoint.setOnClickListener               { addSymbolToEquation(".") }
-        btnNum0.setOnClickListener                { addSymbolToEquation("0") }
-        btnNum1.setOnClickListener                { addSymbolToEquation("1") }
-        btnNum2.setOnClickListener                { addSymbolToEquation("2") }
-        btnNum3.setOnClickListener                { addSymbolToEquation("3") }
-        btnNum4.setOnClickListener                { addSymbolToEquation("4") }
-        btnNum5.setOnClickListener                { addSymbolToEquation("5") }
-        btnNum6.setOnClickListener                { addSymbolToEquation("6") }
-        btnNum7.setOnClickListener                { addSymbolToEquation("7") }
-        btnNum8.setOnClickListener                { addSymbolToEquation("8") }
-        btnNum9.setOnClickListener                { addSymbolToEquation("9") }
+        btnClear.setOnClickListener {
+            tvEquation.text = ""
+            tvResult.text = ""
+        }
+
+        btnBack.setOnClickListener {
+            tvEquation.text = tvEquation.text.dropLast(1)
+            tvResult.text = calculateResult()?.toString() ?: ""
+        }
+        btnAdd.setOnClickListener { addOperatorToEquation("+") }
+        btnSubtract.setOnClickListener { addOperatorToEquation("-") }
+        btnMultiply.setOnClickListener { addOperatorToEquation("*") }
+        btnDivid.setOnClickListener { addOperatorToEquation("/") }
+//        btnOpenParenthesis.setOnClickListener     { addSymbolToEquation("(") }
+//        btnClosingParenthesis.setOnClickListener  { addSymbolToEquation(")") }
+        btnPoint.setOnClickListener { addSymbolToEquation(".") }
+        btnNum0.setOnClickListener { addSymbolToEquation("0") }
+        btnNum1.setOnClickListener { addSymbolToEquation("1") }
+        btnNum2.setOnClickListener { addSymbolToEquation("2") }
+        btnNum3.setOnClickListener { addSymbolToEquation("3") }
+        btnNum4.setOnClickListener { addSymbolToEquation("4") }
+        btnNum5.setOnClickListener { addSymbolToEquation("5") }
+        btnNum6.setOnClickListener { addSymbolToEquation("6") }
+        btnNum7.setOnClickListener { addSymbolToEquation("7") }
+        btnNum8.setOnClickListener { addSymbolToEquation("8") }
+        btnNum9.setOnClickListener { addSymbolToEquation("9") }
 
     }
 
     private fun addSymbolToEquation(symbol: String, isAction: Boolean = false) {
-        if (isAction) {
-            btnAdd.isEnabled = false
-            btnSubtract.isEnabled = false
-            btnMultiply.isEnabled = false
-            btnDivid.isEnabled = false
-        } else {
-            btnAdd.isEnabled = true
-            btnSubtract.isEnabled = true
-            btnMultiply.isEnabled = true
-            btnDivid.isEnabled = true
-        }
-
-        if (symbol === "(") {
-            btnClosingParenthesis.isEnabled = true
-            numParentheses += 1
-        } else if( symbol === ")") {
-            numParentheses -= 1
-            if (numParentheses === 0)
-                btnClosingParenthesis.isEnabled = false
-        }
-
         tvEquation.text = tvEquation.text.toString() + symbol
+        tvResult.text = calculateResult()?.toString() ?: ""
+    }
+
+    private fun calculateResult(): Double? {
+        var result: Double? = null
+        try {
+            val equation = ExpressionBuilder(tvEquation.text.toString()).build()
+            result = equation.evaluate()
+//            tvResult.text = result.toString()
+        } catch (e: Exception) {
+//            tvResult.text = "ERROR"
+        }
+        return result
+    }
+
+    private fun isArithmetic(operator: String): Boolean {
+        return operator == "+" || operator == "-" || operator == "/" || operator == "*"
+    }
+
+    private fun addOperatorToEquation(operator: String) {
+        Log.d("[addOperatorToEquation]", operator)
+        val equation = tvEquation.text.toString()
+
+        if (equation.isEmpty()) {
+            // only symbol allowed to stand at the beginning of the equation is a '-'
+            if (operator == "-") {
+                tvEquation.text = equation + operator
+            }
+            return
+        }
+
+        val lastChar = equation.takeLast(1)
+        val secondLastChar = equation.takeLast(2).take(1)
+
+        if (isArithmetic(lastChar)) {
+            if (operator == "-") {
+                if (lastChar == "+") {
+                    tvEquation.text = equation.dropLast(1) + operator
+                } else if (lastChar == "*" || lastChar == "/") {
+                    tvEquation.text = equation + operator
+                } else {
+                    return // chaining '-' should not be possible
+                }
+            } else if (operator == "+") {
+                if (isArithmetic(secondLastChar)) {
+                    tvEquation.text = equation.dropLast(2) + operator
+                } else {
+                    tvEquation.text = equation.dropLast(1) + operator
+                }
+            } else if (operator == "*") {
+                if (isArithmetic(secondLastChar)) {
+                    tvEquation.text = equation.dropLast(2) + operator
+                } else {
+                    tvEquation.text = equation.dropLast(1) + operator
+                }
+            } else if (operator == "/") {
+                if (isArithmetic(secondLastChar)) {
+                    tvEquation.text = equation.dropLast(2) + operator
+                } else {
+                    tvEquation.text = equation.dropLast(1) + operator
+                }
+            }
+        } else {
+            Log.d("[Noo]", secondLastChar)
+            tvEquation.text = equation + operator
+        }
     }
 }
